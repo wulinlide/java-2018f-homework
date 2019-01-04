c50520e5f08e587314dcfae49ca3af1517a6d70e
wulinlide <695160225@qq.com>
2019-01-04 21:09:49 +0800

    fix maven

--- Final Project/吴琳丽-161220140/final_huluwa/src/main/java/company/Creature/Cheer.java
+++
@@ -1,6 +0,0 @@
-package company.Creature;
-import company.BattleField.Position;
-
-public interface Cheer {
-    void creatureCheer(Position[][] Field, int x, int y);//小写
-}
--- Final Project/吴琳丽-161220140/final_huluwa/src/main/java/company/Formation/Formation.java
+++
@@ -1,9 +0,0 @@
-package company.Formation;
-
-import company.BattleField.Position;
-import company.Creature.Creature;
-
-public interface Formation {
-    boolean ifEmpty(Position[][] Field, int x, int y);
-    void setCreatures(Position[][] Field, int x, int y, Creature[] creatures);
-}
